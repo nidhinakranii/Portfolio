0a2a760c674991c22102aa71dbcd8c194445c34e
nidhinakranii <nidhinakranii@gmail.com>
2024-06-23 15:41:58 -0400

    contact.js changed

--- src/pages/contact/Skill.js
+++ src/pages/contact/Skill.js
@@ -1,15 +1,15 @@
-// Contact.jsx
+// Skill.jsx
 
 import React, { useEffect, useRef } from 'react';
 import { Footer } from 'components/Footer';
 import { Section } from 'components/Section';
-import styles from './Contact.module.css'; // Import local module CSS
+import styles from './Skill.module.css'; // Import local module CSS
 import { DecoderText } from 'components/DecoderText';
 import { Heading } from 'components/Heading';
 
 import Card from 'react-bootstrap/Card';
 
-export const Contact = () => {
+export const Skill = () => {
   const cardContainerRef = useRef(null);
 
   useEffect(() => {
@@ -41,8 +41,8 @@ export const Contact = () => {
   ];
 
   return (
-    <div className={styles.contactContainer}>
-      <Section className={styles.contact}>
+    <div className={styles.skillContainer}>
+      <Section className={styles.skill}>
         <div ref={cardContainerRef} className={`${styles.cardContainer} cardContainer`}>
           {skills.map((skill, index) => (
             <Card key={index} className={styles.card}>
