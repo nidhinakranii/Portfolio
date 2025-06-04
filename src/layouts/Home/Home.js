c430b94dec1596621e75e6a8033c5f90bdc5e6f2
Metis <metis@Nidhis-MacBook-Pro.local>
2025-06-04 16:21:11 -0400

    updated

--- src/layouts/Home/Home.js
+++ src/layouts/Home/Home.js
@@ -3,7 +3,7 @@ import aliagentimg from 'assets/Aiagent.png';
 import soliditygasoptimg from 'assets/soliditygasopt.png';
 import educatorworkshopimg from 'assets/Educatorworkshop.png';
 import stakingimg from 'assets/staking-compound-1.png';
-import cryptoloopimg from 'assets/cryptoloop.png';
+import cryptoloopimg from 'assets/cryptoloop-1.png';
 import { Footer } from 'components/Footer';
 import { Meta } from 'components/Meta';
 import { Intro } from 'layouts/Home/Intro';
